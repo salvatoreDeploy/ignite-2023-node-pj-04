f30221dc45574cbb96673b574db717455de205a3
salvatoreDeploy <liderhenrique@gmail.com>
2023-12-07 12:17:30 -0300

    Commit: Aggregates & Watched Lists - Pattern: WatchedList

--- src/core/entities/watched-list.spec.ts
+++ src/core/entities/watched-list.spec.ts
@@ -31,4 +31,36 @@ describe('watched list', () => {
     expect(list.getRemovedItems()).toEqual([2])
     expect(list.getRemovedItems()).toHaveLength(1)
   })
+
+  it('Should be able to add an item even if it was removed before', () => {
+    const list = new NumberWatchedList([1, 2, 3])
+
+    list.remove(2)
+    list.add(2)
+
+    expect(list.currentItems).toHaveLength(3)
+    expect(list.getRemovedItems()).toEqual([])
+    expect(list.getNewItems()).toEqual([])
+  })
+
+  it('Should be able to remove an item even if it was added before', () => {
+    const list = new NumberWatchedList([1, 2, 3])
+
+    list.add(4)
+    list.remove(4)
+
+    expect(list.currentItems).toHaveLength(3)
+    expect(list.getRemovedItems()).toEqual([])
+    expect(list.getNewItems()).toEqual([])
+  })
+
+  it('Should be able to update watched list items', () => {
+    const list = new NumberWatchedList([1, 2, 3])
+
+    list.update([1, 3, 5])
+
+    expect(list.currentItems).toHaveLength(3)
+    expect(list.getRemovedItems()).toEqual([2])
+    expect(list.getNewItems()).toEqual([5])
+  })
 })
